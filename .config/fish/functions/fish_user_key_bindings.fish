9dc3522007cfe302fc979d849fec60aac692663e
uzimith <uzimith.x9@gmail.com>
2017-02-13 13:58:42 +0900

    add sm keybind

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -5,6 +5,7 @@ function fish_user_key_bindings
   bind -M default -m insert sg peco_select_repository
   bind -M default -m insert sG peco_open_repository
   bind -M default -m insert sr peco_recent_directory
+  bind -M default -m insert sm peco_select_history
   bind -M default -m default 's*' peco_ripgrep_vim
   bind -M default -m insert sd peco_ls
 
